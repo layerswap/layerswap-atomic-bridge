8cbb9392cf06d7d170fd2e9d85d13aa05d572286
Emgevorgyan <emmanuelgevorgyan@gmail.com>
2024-05-20 18:54:48 +0400

    feat(evm): Add user script

--- examples/evm/src/scripts/user.ts
+++ examples/evm/src/scripts/user.ts
@@ -4,7 +4,8 @@ import {
   LP_ADDRESS,
   OPTIMISM_RPC_ENDPOINT,
   OPTIMISM_WSS_ENDPOINT,
-  PRE_HTLC_CONTRACT_ADDRESS,
+  ARB_HTLC_CONTRACT_ADDRESS,
+  OP_HTLC_CONTRACT_ADDRESS,
   USER_PRIVATE_KEY,
   USER_TIMELOCK_SECONDS,
 } from '../config';
@@ -12,17 +13,22 @@ import { checkConfirmations, getContractEventListener, log, subscribeToEvent } f
 import { ChainID, AssetID, PreEvmHtlc } from '@layerswap/evm';
 
 (async () => {
-  const client = new PreEvmHtlc(ARBITRUM_RPC_ENDPOINT, PRE_HTLC_CONTRACT_ADDRESS);
-  const clientOp = new PreEvmHtlc(OPTIMISM_RPC_ENDPOINT, PRE_HTLC_CONTRACT_ADDRESS);
+  const client = new PreEvmHtlc(ARBITRUM_RPC_ENDPOINT, ARB_HTLC_CONTRACT_ADDRESS);
+  const clientOp = new PreEvmHtlc(OPTIMISM_RPC_ENDPOINT, OP_HTLC_CONTRACT_ADDRESS);
 
   const accounts = client.web3.eth.accounts;
   const userAddress = accounts.wallet.add(USER_PRIVATE_KEY).address;
   const timelock = Math.floor(Date.now() / 1000) + USER_TIMELOCK_SECONDS; // Current time + 1 hour
+  let totalGasUsed = 0;
+  const startTime = Date.now();
+  const gasPriceWei = await client.web3.eth.getGasPrice();
 
   const createPre = async () => {
     log('User creating transaction on Arbitrum...');
 
     const receipt = await client.createPre(
+      [ChainID.OPTIMISM],
+      [LP_ADDRESS],
       ChainID.ARBITRUM,
       AssetID.ETH,
       userAddress,
@@ -37,6 +43,7 @@ import { ChainID, AssetID, PreEvmHtlc } from '@layerswap/evm';
     );
 
     log(`CreatePre transaction sent: ${receipt.transactionHash}`);
+    totalGasUsed += receipt.gasUsed;
 
     await listenForLpTransaction();
   };
@@ -56,21 +63,6 @@ import { ChainID, AssetID, PreEvmHtlc } from '@layerswap/evm';
     }
   };
 
-  const processTransferClaimedEvent = async (event: any) => {
-    const { transactionHash, returnValues } = event;
-
-    log('\n');
-    log(`"EtherTransferClaimed" event detected on Optimism`);
-    log(`Hash: ${transactionHash}`);
-
-    try {
-      await checkConfirmations(clientOp, transactionHash);
-      await createRedeemTransaction(returnValues);
-    } catch (error) {
-      log('Error checking confirmations:', error);
-    }
-  };
-
   const createConvertPTransaction = async (returnValues: any) => {
     const { hashlock, phtlcID } = returnValues;
     log('The user is preparing to make a "convertP" transaction on Arbitrum (phtlcID: %s)', phtlcID);
@@ -81,36 +73,27 @@ import { ChainID, AssetID, PreEvmHtlc } from '@layerswap/evm';
       });
 
       log(`ConvertP transaction sent: ${receipt.transactionHash}`);
+      totalGasUsed += receipt.gasUsed;
+
+      await checkConfirmations(client, receipt.transactionHash);
+      const gasUsedBN = client.web3.utils.toBN(totalGasUsed);
+      const totalCostWeiBN = client.web3.utils.toBN(gasPriceWei).mul(gasUsedBN);
+      const totalCostEthBN = client.web3.utils.fromWei(totalCostWeiBN.toString(), 'ether');
+
+      log(`Total fee: ${totalCostEthBN.toString()} ETH`);
+      log(`Execution Time: ${(Date.now() - startTime) / 1000} seconds`);
+      log(`Done!`);
+      process.exit(0);
     } catch (error) {
       log(`Error creating convertP transaction: ${error}`);
     }
   };
 
-  const createRedeemTransaction = async (returnValues: any) => {
-    log('The user is preparing to make a "redeem" transaction on Arbitrum...');
-    const { htlcId } = returnValues;
-
-    try {
-      const htlcInfo = await clientOp.getContractInfo(htlcId);
-      const { transactionHash } = await client.redeem(htlcId, htlcInfo.secret, {
-        senderAddress: userAddress,
-      });
-      log(`Redeem transaction sent: ${transactionHash}`);
-
-      await checkConfirmations(client, transactionHash);
-      log(`Done! Burger time! 🍔🍟🥤`);
-      process.exit(0);
-    } catch (error) {
-      log(`Error creating redeem transaction: ${error}`);
-    }
-  };
-
   const listenForLpTransaction = async () => {
-    const contract = await getContractEventListener(OPTIMISM_WSS_ENDPOINT);
+    const contract = await getContractEventListener(OPTIMISM_WSS_ENDPOINT, OP_HTLC_CONTRACT_ADDRESS);
     console.log('\n');
     log(`The user has initiated listening for LP transactions on Arbitrum....`);
     subscribeToEvent(contract, 'EtherTransferInitiated', processTransferInitiatedEvent);
-    subscribeToEvent(contract, 'EtherTransferClaimed', processTransferClaimedEvent);
   };
 
   await createPre();
